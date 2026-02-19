3c3f133c70840738362ee5df64180cee1b28e0a9
bretmalkin <bret@bretmalkin.com>
2026-02-19 15:52:22 -0700

    Site build

--- blocks/featured-post-grid/featured-post-grid.php
+++ blocks/featured-post-grid/featured-post-grid.php
@@ -29,6 +29,8 @@ $block_alignment = get_field('block_alignment');
 $header_color = get_field('header_color');
 $text_and_button_color = get_field('text_and_button_color');
 $featured_post_selections = get_field('featured_post_selections');
+$post_type_selection = get_field('post_type');
+$post_type_object_selection = get_post_type_object($post_type_selection);
 $call_to_action = get_field('call_to_action');
 $call_to_action_label = get_field('call_to_action_label');
 if( $call_to_action ): 
@@ -143,6 +145,84 @@ if ($block_alignment == 'left') {
                     </div>
                 <?php endforeach; ?>
             </div>
+        <?php else : ?>
+            <?php 
+            $args = array(
+                'post_type'      => $post_type_selection, 
+                'posts_per_page' => 3,
+                'post__not_in'   => array( get_the_ID() ), 
+                'orderby'        => 'date',
+                'order'          => 'DESC'
+            );
+            $related_query = new WP_Query( $args );
+
+            if ($related_query->have_posts()) : ?>
+                <div class="row">
+                    <?php while ($related_query->have_posts()) : $related_query->the_post(); 
+                        $permalink = get_permalink(get_the_ID());
+                        $title = get_the_title(get_the_ID());
+                        $summary_excerpt = get_field('summary_excerpt', get_the_ID());
+                        $excerpt = get_the_excerpt(get_the_ID());
+                        $thumbnail_id = get_post_thumbnail_id(get_the_ID());
+                        $featured_image_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                        $alt_text = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+                        $categories = get_the_category(get_the_ID());
+                        $industries = get_the_terms( get_the_ID(), 'industries' );
+                        ?>
+                        <div class="col-12 col-md-6 col-lg-4 featured-post-grid__wrap position-relative pb-10 pb-md-8 pb-lg-0">
+                            <div class="overlay object-fit-cover featured-post-grid__wrap__img">
+                                <?php if ($featured_image_url) : ?>
+                                    <img src="<?php echo esc_url( $featured_image_url ); ?>" alt="<?php echo esc_attr( $alt_text ); ?>" class="w-100"/>
+                                <?php else : ?>
+                                    <img src="/wp-content/uploads/2026/01/default-social-sharing-image.jpeg" alt="Post Placeholder Image" class="w-100"/>
+                                <?php endif; ?>
+                            </div>
+                            <?php if ($title) : ?>
+                                <h3>
+                                    <?php echo $title; ?>
+                                </h3>
+                            <?php endif; ?>
+                            <?php if ($categories) : ?>
+                                <?php $lastCat = end($categories); ?>
+                                <p class="text-sm-bold mt-2 text-blue-ada font-tertiary">
+                                    <?php foreach( $categories as $category): ?>
+                                        <?php echo esc_html( $category->name );
+                                            if ($category !== $lastCat) {
+                                                echo ', ';
+                                            }
+                                        ?>
+                                    <?php endforeach; ?>
+                                </p>
+                            <?php endif; ?>
+                            <?php if ( ! empty( $industries ) && ! is_wp_error( $industries ) ) : ?>
+                                <?php $lastInd = end($industries); ?>
+                                <p class="text-sm-bold mt-2 text-blue-ada font-tertiary">
+                                    <?php foreach( $industries as $industry): ?>
+                                        <?php echo esc_html( $industry->name );
+                                            if ($industry !== $lastInd) {
+                                                echo ', ';
+                                            }
+                                        ?>
+                                    <?php endforeach; ?>
+                                </p>
+                            <?php endif; ?>
+                            <?php if ($summary_excerpt) : ?>
+                                <div class="text-md-regular mt-4 text-black font-tertiary featured-post-grid__wrap__excerpt">
+                                    <?php echo $summary_excerpt; ?>
+                                </div>
+                            <?php endif; ?>
+                            <?php if ($excerpt) : ?>
+                                <p class="text-md-regular mt-4 text-black font-tertiary featured-post-grid__wrap__excerpt">
+                                    <?php echo $excerpt; ?>
+                                </p>
+                            <?php endif; ?>
+                            <a class="button button--steel-underline" href="<?php echo $permalink; ?>" aria-label="Open post">
+                                Read <?php echo esc_html($post_type_object_selection->labels->singular_name); ?>
+                            </a>
+                        </div>
+                    <?php endwhile; ?>
+                </div>
+            <?php endif; ?>
         <?php endif; ?>
         <div class="row">
             <div class="col-12 mt-lg-10<?php echo $buttonPosition; ?>">
